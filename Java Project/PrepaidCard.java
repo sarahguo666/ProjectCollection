853c13d15cbf943a4bc7d325b69fa377262c452f
sarahguo666 <38124042+sarahguo666@users.noreply.github.com>
2018-12-06 23:01:00 -0500

    Update PrepaidCard.java

--- Java Project/PrepaidCard.java
+++ Java Project/PrepaidCard.java
@@ -1,5 +1,5 @@
 /**
- * Created by Leo on 11/29/17.
+ * Created by Sarah on 11/29/17.
  */
 public class PrepaidCard extends BankCard{
     public PrepaidCard(String cardHolder, long cardNumber, double balance){
